4fa5daeac23a629108402bedf4a388b579dc5d1e
Charles Nicholson <charles.nicholson@gmail.com>
2022-07-29 11:17:11 -0400

    delete comment

--- linfifo_os.h
+++ linfifo_os.h
@@ -2,8 +2,6 @@
 
 #include "linfifo/linfifo.h"
 
-// Internal API for OS-specific detail functions
-
 size_t linfifo_os_mem_page_size(void);
 linfifo_retval_t linfifo_os_mbuf_create(linfifo_t *lf);
 linfifo_retval_t linfifo_os_mbuf_free(linfifo_t *lf);
