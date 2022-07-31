1cb8ddff78632f0104eb40d67c0997dd19d18d7a
Charles Nicholson <charles.nicholson@gmail.com>
2022-07-30 21:50:15 -0400

    better tests for create

--- tests/test_linfifo_create.cc
+++ tests/test_linfifo_create.cc
@@ -21,10 +21,36 @@ TEST_CASE("linfifo_create") {
   REQUIRE(lf.tail == 0);
   REQUIRE(lf.seat);
 
-  SUBCASE("mirror buf") {
-    char *buf = static_cast<char *>(lf.seat);
-    memset(&buf[lf.capacity], 0, lf.capacity);
+  unsigned char *buf = static_cast<unsigned char *>(lf.seat);
+  memset(buf, 0, lf.capacity * 2);
+
+  SUBCASE("mirror buf: first byte first half mirrors to first byte second half") {
+    buf[0] = 0xA5;
+    REQUIRE(buf[lf.capacity] == 0xA5);
+  }
+
+  SUBCASE("mirror buf: first byte second half mirrors to first byte first half") {
+    buf[lf.capacity] = 0x5A;
+    REQUIRE(buf[0] == 0x5A);
+  }
+
+  SUBCASE("mirror buf: last byte first half mirrors to last byte second half") {
+    buf[lf.capacity - 1] = 0xA5;
+    REQUIRE(buf[(lf.capacity * 2) - 1] == 0xA5);
+  }
+
+  SUBCASE("mirror buf: last byte second half mirrors to last byte first half") {
+    buf[(lf.capacity * 2) - 1] = 0xE3;
+    REQUIRE(buf[lf.capacity - 1] == 0xE3);
+  }
+
+  SUBCASE("mirror buf: full range first half mirrors to full range second half") {
     std::iota(buf, buf + lf.capacity, 0);
+    REQUIRE(!memcmp(buf, buf + lf.capacity, lf.capacity));
+  }
+
+  SUBCASE("mirror buf: full range second half mirrors to full range first half") {
+    std::iota(buf + lf.capacity, buf + (lf.capacity * 2) - 1, 0);
     REQUIRE(!memcmp(buf, buf + lf.capacity, lf.capacity));
   }
 
