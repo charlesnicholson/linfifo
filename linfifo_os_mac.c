e894fc1dd38d9503accd4297fb746c25bfc17bdb
Charles Nicholson <charles.nicholson@gmail.com>
2022-07-31 21:23:12 -0400

    shorten the mach vm stuff a bit

--- linfifo_os_mac.c
+++ linfifo_os_mac.c
@@ -3,7 +3,7 @@
 #include <mach/mach.h>
 #include <mach/vm_map.h>
 
-_Static_assert(sizeof(void *) >= sizeof(mem_entry_name_port_t), "");
+_Static_assert(KERN_SUCCESS == 0, "");
 _Static_assert(sizeof(void *) == sizeof(vm_address_t), "");
 
 size_t linfifo_os_mem_page_size(void) { return vm_page_size; }
@@ -12,48 +12,37 @@ linfifo_retval_t linfifo_os_mbuf_create(linfifo_t *lf) {
   if (!lf) { return LINFIFO_RETVAL_ERR_ARG; }
   if (lf->capacity != trunc_page(lf->capacity)) { return LINFIFO_RETVAL_ERR_ARG; }
 
+  // Cache verbose constants for less typing.
+  mach_port_t const self = mach_task_self();
+  int const ow = VM_FLAGS_FIXED | VM_FLAGS_OVERWRITE;
+  int const rw = VM_PROT_READ | VM_PROT_WRITE;
+  size_t cap = lf->capacity;
+
+  // Allocate space for both buffer and mirror.
   vm_address_t addr;
-  if (vm_allocate(mach_task_self(),
-                  &addr,
-                  lf->capacity * 2,
-                  VM_FLAGS_ANYWHERE) != KERN_SUCCESS) {
+  if (vm_allocate(self, &addr, cap * 2, VM_FLAGS_ANYWHERE)) {
     return LINFIFO_RETVAL_ERR_NO_MEM;
   }
 
   lf->seat = (void *)addr;
 
-  if (vm_allocate(mach_task_self(),
-                  &addr,
-                  lf->capacity,
-                  VM_FLAGS_FIXED | VM_FLAGS_OVERWRITE) != KERN_SUCCESS) {
+  // Overwrite allocation to just buffer size, without freeing the mirror space.
+  if (vm_allocate(self, &addr, cap, ow)) {
     vm_deallocate(mach_task_self(), addr, lf->capacity * 2);
     return LINFIFO_RETVAL_ERR_OS;
   }
 
-  mem_entry_name_port_t mapping_port;
-  if (mach_make_memory_entry(mach_task_self(),
-                             &lf->capacity,
-                             addr,
-                             VM_PROT_READ | VM_PROT_WRITE,
-                             &mapping_port,
-                             0) != KERN_SUCCESS) {
+  // Create a memory port for just the buffer size.
+  mem_entry_name_port_t p;
+  if (mach_make_memory_entry(self, &cap, addr, rw, &p, 0)) {
     vm_deallocate(mach_task_self(), addr, lf->capacity * 2);
     return LINFIFO_RETVAL_ERR_OS;
   }
 
-  vm_address_t mirror_addr = addr + lf->capacity;
-  if (vm_map(mach_task_self(),
-             &mirror_addr,
-             lf->capacity,
-             0,
-             VM_FLAGS_FIXED | VM_FLAGS_OVERWRITE,
-             mapping_port,
-             0,
-             VM_INHERIT_SHARE,
-             VM_PROT_READ | VM_PROT_WRITE,
-             VM_PROT_READ | VM_PROT_WRITE,
-             VM_INHERIT_COPY) != KERN_SUCCESS) {
-    vm_deallocate(mapping_port, addr, lf->capacity * 2);
+  // Overwrite-remap the mirror space to the new original-buffer memory port.
+  vm_address_t mirror = addr + cap;
+  if (vm_map(self, &mirror, cap, 0, ow, p, 0, VM_INHERIT_SHARE, rw, rw, VM_INHERIT_COPY)) {
+    vm_deallocate(p, addr, cap * 2);
     return LINFIFO_RETVAL_ERR_OS;
   }
 
@@ -62,7 +51,7 @@ linfifo_retval_t linfifo_os_mbuf_create(linfifo_t *lf) {
 
 linfifo_retval_t linfifo_os_mbuf_free(linfifo_t *lf) {
   if (!lf || !lf->seat) { return LINFIFO_RETVAL_ERR_ARG; }
-  return vm_deallocate(mach_task_self(), (vm_address_t)lf->seat, lf->capacity * 2) ==
-    KERN_SUCCESS ? LINFIFO_RETVAL_SUCCESS : LINFIFO_RETVAL_ERR_OS;
+  return vm_deallocate(mach_task_self(), (vm_address_t)lf->seat, lf->capacity * 2) ?
+    LINFIFO_RETVAL_ERR_OS : LINFIFO_RETVAL_SUCCESS;
 }
 
